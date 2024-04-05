bc920beb02523b10f6ef18c85165971c8290dd43
LuizFernandoLeao <luiz.leao@sou.inteli.edu.br>
2024-04-04 23:27:00 -0300

    feat: desenvolvimento dos códigos e ajustes

--- semana9/main.py
+++ semana9/main.py
@@ -1,9 +1,24 @@
-from flask import Flask, render_template
+# Importação das bibliotecas
+from flask import Flask, render_template, request
 from flask_cors import CORS
+from tinydb import TinyDB
+from pydobot import Dobot
 
 app = Flask(__name__)
 
+# Banco de dados
+db = TinyDB('logs.json')
+
 CORS(app)
+
+# Conexão com o robo
+try:
+    robo = Dobot(port='COM3')
+    robo.speed(100)
+    print("Conectado com o robô")
+except:
+    robo = None
+    print(f"Erro ao conectar com o robô")
 
 elementos = [
     {'id': 1, 'nome': 'Elemento 1'},
@@ -18,21 +33,52 @@ elementos = [
     {'id': 10, 'nome': 'Elemento 10'},
 ]
 
-# Retorna o index.html
+# Tela Inicial 
 @app.route('/')
 def index():
+    # Robo desconectado
+    if robo is None:
+        return'<h2>Conecte o robô, ou entre na rota /log<h2>'
     return render_template('index.html')
 
+# Robo conectado
+if robo is not None:
+    
+    # Header
+    @app.route('/pegar-header')
+    def pegar_header():
+        return '<header id="header">Header</header>'
 
-# Retorna a base do header com o HTMX
-@app.route('/pegar-header')
-def pegar_header():
-    return '<header id="header">Header</header>'
+    # Elementos
+    @app.route('/pegar-elementos')
+    def pegar_elementos():
+        return render_template('elementos.html', elementos=elementos)
 
-# Retorna um conjunto de elementos no formato de uma lista de cards
-@app.route('/pegar-elementos')
-def pegar_elementos():
-    return render_template('elementos.html', elementos=elementos)
-
+    @app.route('/controlar', methods=['POST'])
+    def mover():
+        if request.method == 'POST':
+            x = request.args.get('x')
+            y = request.args.get('y')
+            z = request.args.get('z')
+            r = request.args.get('r')
+            robo.move_to(float(x),float(y),float(z),float(r))
+            return f'<h2>X: {x}, Y: {y}, Z: {z}</h2>'
+    
+    @app.route('/posicao')
+    def posicao():
+        db.insert({'log': 'GET da /posicao'})
+        return f'<h2>{robo.pose()}<h2>'
+    
+    @app.route('/print')
+    def printar():
+        db.insert({'log':'GET na /print'})
+        return '<h2>print</h2>'
+    
+@app.route('/log')
+def exibir_logs():
+    logs = db.all()
+    print(logs)
+    return render_template('logs.html', logs=logs)
+   
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=8000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
--- semana9/readme.md
+++ semana9/readme.md
@@ -5,3 +5,12 @@
 ## 📜 Descrição
 
 &emsp;Esta atividade tem por objetivo realizar uma integração do sistema construído com Flask e o robô físico. O estudante deverá construir um sistema que realiza o log dos comandos enviados por uma interface gráfica construída com HTMX e servida com Flask em um servidor local. Neste servidor, o robô físico deve estar conectado. Quando o robô não estiver conectado, apenas as funcionalidades de visualização do log devem estar disponíveis no sistema
+
+## 🔧 Instalação
+
+&emsp;É possível fazer a instalação do sistema a partir dos seguintes passos:
+
+- Instale o repositório na sua máquina
+- Instale o ambiente virtual venv, a partir do comando: python -m venv venv
+- Instale as bibliotecas necessarias, a partir do comando: pip install -r .\requirements.txt
+- Execute o sistema, a partir do comando: python main.py
